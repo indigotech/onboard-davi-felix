ac6ee441c2459543483cc347a2a092f49993f567
davifelix5 <davi.felix@taqtile.com.br>
2024-05-07 12:59:29 -0300

    :sparkles: fetch users from server on Home component render

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,24 @@
-import {ApolloClient, InMemoryCache} from '@apollo/client';
+import {ApolloClient, createHttpLink, InMemoryCache} from '@apollo/client';
+import {setContext} from '@apollo/client/link/context';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const httpLink = createHttpLink({
+  uri: 'https://template-onboarding-node-sjz6wnaoia-uc.a.run.app/graphql',
+});
+
+const authLink = setContext(async (_, {headers}) => {
+  // get the authentication token from local storage if it exists
+  const token = await AsyncStorage.getItem('ONBOARDING-APP:accessToken');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token || '',
+    },
+  };
+});
 
 export const client = new ApolloClient({
-  uri: 'https://template-onboarding-node-sjz6wnaoia-uc.a.run.app/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
